6a9cedd23d78742c6158c50310941fc79a896d96
Paul J. Lucas <paul@lucasmail.org>
2020-12-01 08:12:02 -0800

    Removed dircolors.sh.

--- lib/dircolors/dircolors.sh
+++
@@ -1,2 +0,0 @@
-LS_COLORS='rs=0:bd=35:cd=35:di=36:mh=00:do=35:ex=32:pi=35:fi=00:ln=34:no=00:or=31:so=35:su=32;01:*.am=33:*.c=33:*.cc=33:*.cpp=33:*.cxx=33:*.groovy=33:*.h=33:*.hpp=33:*.hxx=33:*.in=33:*.java=33:*.js=33:*.l=33:*.lpp=33:*.m=33:*.m4=33:*.mm=33:*.pl=33:*.pm=33:*.sh=33:*.tcc=33:*.xs=33:*.y=33:*.ypp=33:*README=31:*INSTALL=31:';
-export LS_COLORS
